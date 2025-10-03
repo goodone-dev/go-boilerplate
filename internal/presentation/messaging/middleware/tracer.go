fc68f33cf4a2d8b5da36a9203667b33fa19f7bd8
Bagus Abdul Kurniawan <bagus.ak95@gmail.com>
2025-10-03 16:40:16 +0700

    fix: span name

--- internal/presentation/messaging/middleware/tracer.go
+++ internal/presentation/messaging/middleware/tracer.go
@@ -9,7 +9,7 @@ import (
 
 func TracerMiddleware[T any](topic string, handler func(context.Context, T) error) func(T) {
 	return func(msg T) {
-		ctx, span := tracer.SpanCustomName(fmt.Sprintf("EVENT %s", topic)).StartSpan(context.Background())
+		ctx, span := tracer.SpanCustomName(fmt.Sprintf("MESSAGE %s", topic)).StartSpan(context.Background())
 
 		err := handler(ctx, msg)
 		defer func() {
