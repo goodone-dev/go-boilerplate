085e9c7ac938ff13ee3dd36d51467eba3748e8e9
Bagus Abdul Kurniawan <bagus.ak95@gmail.com>
2025-10-18 13:11:32 +0700

    fix: error message

--- internal/presentation/rest/middleware/error.middleware.go
+++ internal/presentation/rest/middleware/error.middleware.go
@@ -1,7 +1,9 @@
 package middleware
 
 import (
+	"errors"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/goodone-dev/go-boilerplate/internal/config"
@@ -17,21 +19,21 @@ func ErrorMiddleware() gin.HandlerFunc {
 			err := c.Errors.Last().Err
 
 			if e, ok := err.(*error.CustomError); ok {
-				logger.Errorf(c.Request.Context(), e, "failed to process %s %s: %s", c.Request.Method, c.Request.URL.Path, e.Message)
-
 				res := gin.H{"message": e.Message}
 				if len(e.Errors) > 0 && config.ApplicationConfig.Env != config.EnvProd {
 					res["errors"] = e.Errors
 				}
 
+				logger.Errorf(c.Request.Context(), errors.New(strings.Join(e.Errors, ", ")), "failed to process %s %s: %s", c.Request.Method, c.Request.URL.Path, e.Message)
+
 				c.JSON(e.Status, res)
 			} else {
-				logger.Errorf(c.Request.Context(), err, "failed to process %s %s: an unexpected error occurred", c.Request.Method, c.Request.URL.Path)
-
 				res := gin.H{"message": "an unexpected error occurred"}
 				if config.ApplicationConfig.Env != config.EnvProd {
 					res["errors"] = []string{err.Error()}
 				}
+
+				logger.Errorf(c.Request.Context(), err, "failed to process %s %s: an unexpected error occurred", c.Request.Method, c.Request.URL.Path)
 
 				c.JSON(http.StatusInternalServerError, res)
 			}
